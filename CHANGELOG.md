e427ab185900820bb8ce7e1f4f305fc5adc592d1
Cat McGarvey <catrionamcgarvey@hotmail.com>
2019-11-11 16:17:53 +0000

    fix: add new code
    * chore(release): 1.0.0
    
    * fix: add new code

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Changelog
+
+All notable changes to this project will be documented in this file. See [standard-version](https://github.com/conventional-changelog/standard-version) for commit guidelines.
+
+## 1.0.0 (2019-11-11)
+
+
+### Features
+
+* cat cat  ([#18](https://github.com/catmcgarvey/graphql-example-repo/issues/18)) ([517a671](https://github.com/catmcgarvey/graphql-example-repo/commit/517a6714abbad1a311575fdb2d2f519995501366))
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
+const { version } = require("./package.json");
 const Koa = require("koa");
+const logger = require("koa-logger");
 const Router = require("koa-router");
 const app = new Koa();
 const router = new Router();
+app.use(logger());
 
 router.get("/", (ctx, next) => {
-  ctx.body = "Hello World!";
+  ctx.body = "Welcome to the app!";
+});
+
+router.get("/health-check", (ctx, next) => {
+  ctx.body = { version };
 });
 
 app.use(router.routes());
